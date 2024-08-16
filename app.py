aa322bda790f1392773949e1e9a367cdbd438130
Venkatasai-018 <karthik.reddi026@gmail.com>
2024-08-17 02:08:03 +0530

    Added Basic

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-import pandas as pd
+from flask import Flask, url_for, redirect, render_template, url_for, session, logging, request
+
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
